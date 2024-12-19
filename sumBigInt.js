bb831c293987a1e86ebd8d202340916ea5739b66
ASDlikeS <penetresing_test@tutamail.com>
2024-12-20 00:17:53 +0300

    commit "sumBigInt"

---
+++ sumBigInt.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+  //WE CANT USE BIGINT OMG!!!!!----
+  //Alr it's ok for me, so now i'll try to sum last indexes from string and return all string (i won't convert a string to int, this useless);
+
+  result = ``;
+  let carry = 0;
+  let fir = a.length - 1;
+  let sec = b.length - 1;
+
+  while (fir >= 0 || sec >= 0 || carry) {
+    let sum = carry;
+
+    if (fir >= 0) sum += parseInt(a[fir--]);
+    if (sec >= 0) sum += parseInt(b[sec--]);
+
+    carry = Math.floor(sum / 10);
+
+    result = (sum % 10) + result;
+  }
+  return result;
+}
